405e27f596821dc536ba39d3d432e258c13c647f
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-03-08 11:41:45 +0000

    add state to input field

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/InputField.css";
 
-const InputField = props => (
-    <div className="search">
-      <input
+class InputField extends Component  {
+
+  constructor() {
+    super();
+    this.state = {
+      valueOfInput: " "  
+
+    };
+  }
+
+  grabSomeDataFromInput = (event) => {
+    const newValueOfInput = event.target.value;
+    return this.setState({valueOfInput: newValueOfInput })
+  }
+
+  sendCountryToParent = () => {
+    const {sendToMyParent} = this.props;
+    const {valueOfInput} = this.state;
+    
+    return sendToMyParent(valueOfInput);
+      
+  }
+
+  render() {
+      return (
+        <div className="search">
+        <input
           type="text"
           className="search-term"
           placeholder="Show me the country"
-      />
-    <button type="submit" className="search-button">
-        <i className="fa fa-search"/>
-    </button>
-    </div>
-);
+          onChange={this.grabSomeDataFromInput}
+        />
+        <button type="submit" className="search-button"
+        onClick={ this.sendCountryToParent }>
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+      )
+  };
+}
+
 
 export default InputField;
